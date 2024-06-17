091cc842375d70bde6bd66b920885d49dc1496aa
Your Name <you@example.com>
2024-06-17 20:52:35 +0300

    add admin logic

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import asyncio
 import logging
 import sys
 from datetime import datetime
+
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+
 from token_tg import BOT_TOKEN
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+
+ADMIN_PASSWORD = "12431243"
 
 class BaseORM(DeclarativeBase):
     __abstract__ = True
@@ -22,6 +28,7 @@ class User(BaseORM):
     username: Mapped[str] = mapped_column(unique=True)
     fullname: Mapped[str]
     created_at: Mapped[datetime]
+    is_admin: Mapped[bool] = mapped_column(default=False)
 
 
 def db_setup():
@@ -36,6 +43,22 @@ engine, session_factory = db_setup()
 
 dp = Dispatcher()
 
+class AdminSign(StatesGroup):
+    password = State()
+
+@dp.message(AdminSign.password)
+async def handle_password(message: Message, state: FSMContext):
+    if ADMIN_PASSWORD == message.text:
+        with session_factory() as session:
+            user = session.query(User).filter(User.tg_id == message.from_user.id).first()
+            if user:
+                user.is_admin = True
+                session.commit()
+                await message.answer("Вы теперь администратор!")
+    else:
+        await message.answer("Неверный пароль!")
+    await state.clear()
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -49,6 +72,22 @@ async def command_start_handler(message: Message) -> None:
         user = User(tg_id=tg_user.id, username=tg_user.username, fullname=tg_user.full_name, created_at=datetime.now())
         session.add(user)
         session.commit()
+
+@dp.message(Command('admin'))
+async def admin_handler(message: Message, state: FSMContext) -> None:
+    with session_factory() as session:
+        user = session.query(User).filter(User.tg_id == message.from_user.id).first()
+        if not user:
+            return
+        if user.is_admin:
+            await message.answer("Вы уже администратор!")
+            return
+        await state.set_state(AdminSign.password)
+        await message.answer("Введите админский пароль!")
+
+
+
+
 
 async def main() -> None:
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
