826d9d67b331f72cb0cb13a2ff6e18e4e9eb78d9
Your Name <you@example.com>
2024-06-19 21:22:24 +0300

    add delete keyboard

---
+++ test.py
@@ -0,0 +1,18 @@
+import json
+
+import aiohttp
+import asyncio
+
+API_KEY = "526d292effd317f35269014eb5eb66ab"
+city_name = "Voronezh"
+lat = 51.6605982
+lon = 39.2005858
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric&lang=ru') as resp:
+            print(resp.status)
+            print(json.loads(await resp.text()))
+
+
+asyncio.run(main())
